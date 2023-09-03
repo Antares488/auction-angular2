e9f9c93e697c5a44e062ebf3927f1e60867b4c71
a.rosokhatyy <a.rosokhatyy@click2.money>
2023-09-03 17:24:09 +0300

    auction

---
+++ app/components/home/home.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import {Product, ProductService} from '../../services/product-service';
+import {FormControl} from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+@Component({
+    selector: 'auction-home-page',
+    styleUrls: ['/home.css'],
+    templateUrl: 'app/components/home/home.html',
+})
+export default class HomeComponent {
+    products: Product[] = [];
+    titleFilter: FormControl = new FormControl();
+    filterCriteria: string;
+
+    constructor(private productService: ProductService) {
+        this.products = this.productService.getProducts();
+        this.titleFilter.valueChanges
+            .debounceTime(100)
+            .subscribe(
+                value => this.filterCriteria = value,
+                error => console.error(error));
+    }
+}
