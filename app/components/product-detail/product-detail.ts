e5dc60e7964e1d09cd33bc279c4cd4eedf8e178d
a.rosokhatyy <a.rosokhatyy@click2.money>
2023-09-03 22:23:27 +0300

    added formBuilder and validation . search method

--- app/components/product-detail/product-detail.ts
+++ app/components/product-detail/product-detail.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Product, ProductService, Review} from '../../services/product-service';
+import StarsComponent from '../stars/stars';
 
 @Component({
   selector: 'auction-product-page',
@@ -9,10 +10,31 @@ import {Product, ProductService, Review} from '../../services/product-service';
 export default class ProductDetailComponent {
   product: Product;
   reviews: Review[];
+  newComment: string;
+  newRating: number;
+  isReviewHidden: boolean = true;
   constructor(route: ActivatedRoute, productService: ProductService)
   {
     let prodId: number = parseInt(route.snapshot.params['productId']);
     this.product = productService.getProductById(prodId);
     this.reviews = productService.getReviewsForProduct(this.product.id);
   }
+
+  addReview() {
+    let review = new Review(0, this.product.id, new Date(), 'Anonymous', this.newRating, this.newComment);
+    this.reviews = [...this.reviews, review];
+    this.product.rating = this.averageRating(this.reviews);
+    this.resetForm();
+  }
+  averageRating(reviews: Review[]) {
+    let sum = reviews.reduce((average, review) => average + review.rating, 0);
+    return sum / reviews.length;
+  }
+  resetForm() {
+    this.newRating = 0;
+    this.newComment = null;
+    this.isReviewHidden = true;
+  }
 }
+
+
--- app/components/stars/stars.ts
+++ app/components/stars/stars.ts
@@ -1,17 +1,30 @@
-import {Component,  Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 @Component({
-    templateUrl: 'app/components/stars/stars.html',
-    styles: [` .starrating { color: #d17581; }`],
-    selector: 'auction-stars'
+    selector: 'auction-stars',
+    styles: [`.starrating { color: #d17581; }`],
+    templateUrl: 'app/components/stars/stars.html'
 })
-export default class StarsComponent implements OnInit {
-    @Input() count: number = 5;
-    @Input() rating: number = 0;
-    stars: boolean[] = [];
+export default class StarsComponent {
+    private _rating: number;
+    public stars: boolean[];
+    private maxStars: number = 5;
+    @Input() readonly: boolean = true;
 
-    ngOnInit() {
-        for (let i = 1; i <= this.count; i++) {
-            this.stars.push(i > this.rating);
+    @Input() get rating(): number {
+        return this._rating;
+    }
+
+    set rating(value: number) {
+        this._rating = value || 0;
+        this.stars = Array(this.maxStars).fill(true, 0, this.rating);
+    }
+
+    @Output() ratingChange: EventEmitter<number> = new EventEmitter();
+
+    fillStarsWithColor(index) {
+        if (!this.readonly) {
+            this.rating = index + 1;
+            this.ratingChange.emit(this.rating);
         }
     }
 }
